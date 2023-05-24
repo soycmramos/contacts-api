7d8141951d3cbe1081b62ca339267769dd95721d
Carlos Ramos <carlosmarioramos34@gmail.com>
2023-05-24 18:52:33 -0500

    Add deleteContactById feature

---
+++ src/controllers/contacts.deleteById.js
@@ -0,0 +1,63 @@
+import pool from '../db/pool.js'
+
+const deleteContactById = async (req, res) => {
+  const { params, uuid } = req
+  const { id } = params
+
+  try {
+    const [{ affectedRows }] = await pool.query('DELETE FROM contacts WHERE id = ?', id)
+
+    if (!Boolean(affectedRows)) {
+      res
+        .status(404)
+        .header({ 'Content-Type': 'application/json' })
+        .json({
+          status: 'error',
+          code: 404,
+          title: 'NOT_FOUND',
+          message: 'Resource not found',
+          meta: {
+            _timestamp: parseInt(Date.now() / 1000),
+            _requestId: uuid,
+            _requestPath: req.baseUrl + req.path,
+          },
+        })
+      return
+    }
+
+    res
+      .status(200)
+      .header({ 'Content-Type': 'application/json' })
+      .json({
+        status: 'success',
+        code: 200,
+        title: 'OK',
+        message: 'Resource deleted successfully',
+        meta: {
+          _timestamp: parseInt(Date.now() / 1000),
+          _requestId: uuid,
+          _requestPath: req.baseUrl + req.path,
+        },
+      })
+    return
+  } catch (e) {
+    console.error(e)
+    res
+      .status(500)
+      .header({ 'Content-Type': 'application/json' })
+      .json({
+        status: 'error',
+        code: 500,
+        title: 'INTERNAL_SERVER_ERROR',
+        message: 'Something went wrong',
+        meta: {
+          _timestamp: parseInt(Date.now() / 1000),
+          _requestId: uuid,
+          _requestPath: req.baseUrl + req.path,
+        },
+      })
+    return
+  }
+}
+
+export default deleteContactById
--- src/routes/index.js
+++ src/routes/index.js
@@ -7,9 +7,11 @@ import checkHeaders from '../middlewares/check-headers-and-body.js'
 import index from '../controllers/index.js'
 import createContact from '../controllers/contacts.create.js'
 import getContactById from '../controllers/contacts.getById.js'
+import deleteContactById from '../controllers/contacts.deleteById.js'
 
 router.get('/ping', index)
 router.put('/contacts', checkHeaders, createContact)
 router.get('/contacts/:id', checkHeaders, getContactById)
+router.delete('/contacts/:id', checkHeaders, deleteContactById)
 
 export default router
