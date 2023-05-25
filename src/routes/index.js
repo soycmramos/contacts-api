9acf1e0855a6ec20fa93f9c7b045b9f03e1b619d
Carlos Ramos <carlosmarioramos34@gmail.com>
2023-05-25 08:00:13 -0500

    Rename files controllers

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,14 +2,14 @@ import { Router } from 'express'
 const router = Router()
 
 // middlewares
-import checkHeaders from '../middlewares/check-headers-and-body.js'
+import checkHeaders from '../middlewares/check-headers.js'
 
 import index from '../controllers/index.js'
-import createContact from '../controllers/contacts.create.js'
-import getContactById from '../controllers/contacts.getById.js'
-import deleteContactById from '../controllers/contacts.deleteById.js'
-import updateContactById from '../controllers/contacts.updateById.js'
-import getContacts from '../controllers/contacts.getContacts.js'
+import getContacts from '../controllers/getContacts.js'
+import createContact from '../controllers/createContact.js'
+import getContactById from '../controllers/getContactById.js'
+import updateContactById from '../controllers/updateContactById.js'
+import deleteContactById from '../controllers/deleteContactById.js'
 
 router.get('/ping', index)
 router.get('/contacts', checkHeaders, getContacts)
